4564732b39a3756f8b8074a5e4245dc95f8030c0
Sergey Bogdanov <istayeri@gmail.com>
2022-11-30 13:51:06 +0700

    Init project and update gitignore

---
+++ app/src/main/java/com/example/arcoretestapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.arcoretestapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.arcoretestapp.helloar.HelloArActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, HelloArActivity::class.java))
+    }
+}
